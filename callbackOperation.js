e23eb21bb217709d464855ac4ee48b60075544b0
Mohanasubi <mohanasubivenkatachalapathi>
2025-03-13 14:48:25 +0530

    ADD - added the callBackOperation

--- callbackOperation.js
+++ callbackOperation.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const fetchData = (callBack)=>{
+    fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(response=>response.json())
+    .then(data => callBack(data,null))
+    .catch(error => callBack(null,error))
+}
+
+function handleData(data,error){
+  if(error)
+  {
+    console.log(error)
+    return
+  }
+  console.log(data)
+}
+fetchData(handleData)
--- promiseOperation.js
+++ promiseOperation.js
@@ -1,5 +1,5 @@
 const fetchData = () => {
-    return fetch("https://jsonplaceholder.typicode.com/users")
+    return fetch("https://jsonplaceholder.typicode.com/posts")
     .then(response => response.json())
     .then(data=>console.log(data))
 };
